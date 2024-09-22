f87853e77644ac7c5a7c1ad68f47fdaf8bfd9dec
Chandralekha Gadiraju <gadirajuchandralekha@gmail.com>
2024-09-23 00:20:59 +0530

    removed title

--- my-app/src/components/QuizStart.js
+++ my-app/src/components/QuizStart.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import Footer from './QuizFooter';
 import '../styles/QuizStart.scss';
-import arrow from '../images/upArrow.png';
 
 const QuizStart = ({ onStartQuiz }) => {
     return (
         <div className="quiz-start">
-            <div className="title">
-                <img src={arrow} alt="Up arrow" />
-                <span>upraised</span>
-            </div>
             <div className="circle-container">
                 <div className="circle">
                     <div className="circle-text">QUIZ</div>
